cd803398666cfd89c6599fec442c505aaefcc091
Jacob Wert <jacob.wert1@gmail.com>
2024-11-10 09:28:14 -0800

    Modified point.rs to include documentation code for cargo, and started the bearing_line.rs file.

---
+++ src/bearing_line.rs
@@ -0,0 +1,8 @@
+///
+/// This struct defines a BearingLine, which refers to a straight line
+/// that represents the direction or angle from one point to another.
+///
+pub struct BearingLine {
+    pub latitude: f64,
+    pub longitude: f64,
+}
